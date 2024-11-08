8cec0a86f5458ca29c9e471397b447f28c8c99fa
Aericki Ferreira <bboyaericki@gmail.com>
2024-11-08 09:42:09 -0300

    fix: fix de removed file.

---
+++ frontend/src/pages/Cadastro/index.tsx
@@ -0,0 +1,170 @@
+
+import { useState } from 'react';
+import { InputStyled } from '@/components/InputStyled';
+import {
+  Container,
+  Form,
+  FormTitle,
+  MapContainer,
+  Section
+
+} from './styles';
+import { Button } from '@/components/ui/button';
+
+import { LatLngExpression } from 'leaflet';
+import { Marker, TileLayer, useMapEvents } from 'react-leaflet';
+import useGetLocation from '@/hooks/useGetLocation';
+import { useToast } from '@/hooks/use-toast';
+import { getReverseGeocoding } from '@/api/getReverseGeocoding';
+import { createLocation } from '@/api/locationApi';
+
+export default function Cadastrar() {
+
+  const [formValues, setFormValues] = useState({
+    name: '',
+    address: '',
+    city: '',
+    state: '',
+    country: '',
+    whatsapp: '',
+    latitude: 0,
+    longitude: 0,
+
+  });
+
+  const { toast } = useToast();
+  const { coords } = useGetLocation();
+
+  const getAddress = async (lat: number, lng: number) => {
+    try {
+      const data = await getReverseGeocoding(lat, lng);
+      setFormValues({
+        ...formValues,
+        address: data.address.road || '',
+        city: data.address.city || '',
+        state: data.address.state || '',
+        country: data.address.country || '',
+        latitude: lat,
+        longitude: lng
+
+      });
+
+    } catch (error) {
+      console.error('Erro ao obter endereço:', error);
+    }
+  }
+
+  function onSubmit() {
+
+    try {
+      if (formValues.name && formValues.address && formValues.city && formValues.state && formValues.country && formValues.whatsapp && formValues.latitude && formValues.longitude) {
+        toast({
+          title: 'Local de Treino Cadastrado',
+          description: 'Seu local de treino foi cadastrado com sucesso',
+          color: 'green',
+        });
+      }
+      createLocation(formValues);
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  if (!coords) {
+    return <h1>Carregando Localização ...</h1>;
+  }
+
+  const MapClickEvent = () => {
+    useMapEvents({
+      async click(e) {
+        const { lat, lng } = e.latlng;
+        setFormValues({ ...formValues, latitude: lat, longitude: lng });
+        await getAddress(lat, lng);
+        console.log(formValues);
+      },
+
+
+
+    });
+
+    return null;
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit();
+
+      }}>
+        <FormTitle className='font-bold'>Cadastro do Local de Treino</FormTitle>
+
+        <Section>
+          <MapContainer
+            center={
+              {
+                lat: coords[0],
+                lng: coords[1]
+              } as LatLngExpression}
+            zoom={13}
+          >
+            <MapClickEvent />
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            <Marker
+              position={[formValues.latitude, formValues.longitude] as LatLngExpression} />
+
+          </MapContainer>
+
+        </Section>
+
+        <Section className='section flex flex-col gap-5'>
+          Dados
+
+          <InputStyled
+            label="Nome do Local"
+            name="name" placeholder="Ex: Centro de Treinamento de Breaking"
+            value={formValues.name}
+            onChange={setFormValues}
+          />
+
+          <InputStyled
+            label="Informe o endereço completo"
+            name="address" placeholder="Rua Sebastião Anunciato 99"
+            value={formValues.address}
+            onChange={setFormValues}
+
+          />
+          <InputStyled
+
+            label="Informe a cidade"
+            placeholder='Ex:Londrina'
+            name="city"
+            value={formValues.city}
+            onChange={setFormValues}
+          />
+          <InputStyled
+            label='Informe o numero de Telefone'
+            name="whatsapp"
+            placeholder="Ex: 11 99999-9999"
+            value={formValues.whatsapp}
+            onChange={setFormValues}
+            type='tel'
+            pattern='^\d{2} \d{5}-\d{4}$'
+          />
+
+        </Section>
+        <Button
+          style={{
+            marginTop: '1rem',
+            backgroundColor: "#322153",
+            color: "white",
+          }}
+          type='submit'>Cadastrar</Button>
+      </Form>
+    </Container>
+  )
+}
+
