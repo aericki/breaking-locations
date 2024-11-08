c408c6ef6672a19f9dbed4af616567414d6f827c
Aericki Ferreira <bboyaericki@gmail.com>
2024-11-08 09:42:56 -0300

    feat: preparing to save form on database

--- frontend/src/api/locationApi.ts
+++ frontend/src/api/locationApi.ts
@@ -1,20 +1,33 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
 import Axios from 'axios'
+
+export interface Location {
+  name: string
+  latitude: number
+  longitude: number
+  address: string
+  city: string
+  state: string
+  country: string
+  whatsapp: string
+}
 
 export const api = Axios.create({
   baseURL: 'http://localhost:4000/api'
 });
 
 export const fetchLocations = async (city: string = '') => {
-  const reponse = await api.get('/locations', {
+  const response = await api.get('/locations', {
     params: {
       city
     }
   })
-  return reponse.data;
+  return response.data;
 }
 
-export const createLocation = async (locationData: any) => {
+export const createLocation = async (locationData: Location) => {
+
   const response = await api.post('/locations', locationData);
   return response.data
 }
